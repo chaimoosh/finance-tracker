b18fb6fd2b57859e92dd220834737a176f786cc2
Chaim <chaimoosh@gmail.com>
2019-02-11 15:07:22 +0000

    switched to vscode

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,12 @@
 class Stock < ApplicationRecord
     
     def self.new_from_lookup(ticker)
-        looked_up_stock = StockQuote::Stock.quote(ticker)
-        new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.previous_close)
+        begin
+            looked_up_stock = StockQuote::Stock.quote(ticker)
+            new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.previous_close)
+        rescue Exception => e 
+            return nil
+        end
     end 
     
 end
