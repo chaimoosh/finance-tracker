8ee80497de83778ae7afc34ebf03039a81f28af2
Chaim <chaimoosh@gmail.com>
2019-02-18 20:55:24 +0000

    completed app

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,10 +1,21 @@
+# class User::RegistrationsController < Devise::RegistrationsController
+#     before_action :configure_permitted_parameters 
+    
+#     protected
+    
+#     def configure_permitted_parameters 
+#       devise_parameter_sanitizer.permit(:sign_up, keys[:first_name, :last_name])
+#       devise_parameter_sanitizer.permit(:account_update, keys[:first_name, :last_name])
+#     end
+# end
 class User::RegistrationsController < Devise::RegistrationsController
-    before_filter :configure_permitted_parameters 
-    
-    protected
-    
-    def configure_permitted_parameters 
-       devise_parameter_sanitizer.permit(:sign_up, keys[:first_name, :last_name])
-       devise_parameter_sanitizer.permit(:account_update, keys[:first_name, :last_name])
-    end
+  before_action :configure_permitted_parameters
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+  
 end
